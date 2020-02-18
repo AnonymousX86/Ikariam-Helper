3710889da203517d64fa7819198dce4b7aae6db5
AnonymousX86 <53279236+AnonymousX86@users.noreply.github.com>
2020-02-18 21:40:39 +0100

    Update armies.py

--- src/armies.py
+++ src/armies.py
@@ -1,63 +1,463 @@
 minimum = {
     'land': {
-        'hoplici': {'amount': 100},
-        'oszczepnicy': {'amount': 0},
-        'wojownicy': {'amount': 70},
-        'procarze': {'amount': 0},
-        'łucznicy': {'amount': 30},
-        'tarany': {'amount': 0},
-        'katapulty': {'amount': 12},
-        'kucharze': {'amount': 10},
-        'giganty parowe': {'amount': 0},
-        'strzelcy siarkowi': {'amount': 0},
-        'moździerze': {'amount': 0},
-        'żyrokoptery': {'amount': 0},
-        'balonowi bombardierzy': {'amount': 0},
-        'medycy': {'amount': 0}
+        'hoplici': {
+            'amount': 100,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'oszczepnicy': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'wojownicy': {
+            'amount': 70,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'procarze': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'łucznicy': {
+            'amount': 30,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'tarany': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'katapulty': {
+            'amount': 12,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'kucharze': {
+            'amount': 10,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'giganty parowe': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'strzelcy siarkowi': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'moździerze': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'żyrokoptery': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'balonowi bombardierzy': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'medycy': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        }
     },
     'fleet': {
-        'okręt z miotaczem ognia': {'amount': 30},
-        'okręt z taranem': {'amount': 20},
-        'balista': {'amount': 0},
-        'taran parowy': {'amount': 0},
-        'okręt z katapultą': {'amount': 10},
-        'okręt z moździerzem': {'amount': 0},
-        'krążownik rakietowy': {'amount': 0},
-        'okręt podwodny': {'amount': 0},
-        'śmigacz z napędem kołowym': {'amount': 0},
-        'balonowiec': {'amount': 0},
-        'statek pomocniczy': {'amount': 0}
+        'okręt z miotaczem ognia': {
+            'amount': 30,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'okręt z taranem': {
+            'amount': 20,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'balista': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'taran parowy': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'okręt z katapultą': {
+            'amount': 10,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'okręt z moździerzem': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'krążownik rakietowy': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'okręt podwodny': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'śmigacz z napędem kołowym': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'balonowiec': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'statek pomocniczy': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        }
     }
 }
 recommended = {
     'land': {
-        'hoplici': {'amount': 500},
-        'oszczepnicy': {'amount': 0},
-        'wojownicy': {'amount': 400},
-        'procarze': {'amount': 0},
-        'łucznicy': {'amount': 30},
-        'tarany': {'amount': 30},
-        'katapulty': {'amount': 30},
-        'kucharze': {'amount': 30},
-        'giganty parowe': {'amount': 0},
-        'strzelcy siarkowi': {'amount': 84},
-        'moździerze': {'amount': 30},
-        'żyrokoptery': {'amount': 60},
-        'balonowi bombardierzy': {'amount': 90},
-        'medycy': {'amount': 0}
+        'hoplici': {
+            'amount': 500,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'oszczepnicy': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'wojownicy': {
+            'amount': 400,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'procarze': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'łucznicy': {
+            'amount': 30,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'tarany': {
+            'amount': 30,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'katapulty': {
+            'amount': 30,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'kucharze': {
+            'amount': 30,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'giganty parowe': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'strzelcy siarkowi': {
+            'amount': 84,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'moździerze': {
+            'amount': 30,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'żyrokoptery': {
+            'amount': 60,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'balonowi bombardierzy': {
+            'amount': 90,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'medycy': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        }
     },
     'fleet': {
-        'okręt z miotaczem ognia': {'amount': 0},
-        'okręt z taranem': {'amount': 150},
-        'balista': {'amount': 0},
-        'taran parowy': {'amount': 120},
-        'okręt z katapultą': {'amount': 0},
-        'okręt z moździerzem': {'amount': 42},
-        'krążownik rakietowy': {'amount': 60},
-        'okręt podwodny': {'amount': 0},
-        'śmigacz z napędem kołowym': {'amount': 60},
-        'balonowiec': {'amount': 20},
-        'statek pomocniczy': {'amount': 10}
+        'okręt z miotaczem ognia': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'okręt z taranem': {
+            'amount': 150,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'balista': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'taran parowy': {
+            'amount': 120,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'okręt z katapultą': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'okręt z moździerzem': {
+            'amount': 42,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'krążownik rakietowy': {
+            'amount': 60,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'okręt podwodny': {
+            'amount': 0,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'śmigacz z napędem kołowym': {
+            'amount': 60,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'balonowiec': {
+            'amount': 20,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        },
+        'statek pomocniczy': {
+            'amount': 10,
+            'cost': {
+                'wood': 0,
+                'sulfur': 0,
+                'crystal': 0,
+                'wine': 0
+            }
+        }
     }
 }
 # TODO - koszty jednostek
