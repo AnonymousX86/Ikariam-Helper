75c93b99a4ec3be814da168d60d6c25382e79d03
AnonymousX86 <53279236+AnonymousX86@users.noreply.github.com>
2020-02-19 18:26:43 +0100

    Temporarily added "settings" file

---
+++ src/settings.py
@@ -0,0 +1,5 @@
+owner_id = 309270832683679745
+bot_version = '0.5'
+bot_prefix = '.'
+secret_token = 'NjcyNDQ4MjcxNzkyNDcyMDk1.Xk1tBg.bTyguBkitf4L1Ur8A9nGK4NMIEg'
+invite = 'https://discordapp.com/api/oauth2/authorize?client_id=672448271792472095&permissions=19520&scope=bot'
